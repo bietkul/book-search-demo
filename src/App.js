2054ca676818a905324d808a47cc9fd110cc3168
bietkul <kuldepsaxena155@gmail.com>
2019-11-15 19:18:09 +0530

    fix: minor fixes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import {
   ReactiveBase,
   DataSearch,
   ReactiveList,
-  ResultCard,
   SelectedFilters,
   SingleRange,
-  MultiList
+  MultiList,
+  ResultList
 } from "@appbaseio/reactivesearch";
 
 import "./index.css";
@@ -20,19 +20,18 @@ const App = () => (
   >
     <div className="row">
       <div className="col">
-        <div className="filter">
-          <DataSearch
-            title="DataSearch"
-            dataField={"original_title"}
-            componentId="SearchBox"
-            // URLParams
-            react={{
-              and: ["AuthorFilter", "RatingsFilter", "SearchResult"]
-            }}
-          />
-        </div>
-        <div className="filter">
-          <MultiList
+        <DataSearch
+          className="filter"
+          title="DataSearch"
+          componentId="SearchBox"
+          dataField="original_title"
+          URLParams
+          react={{
+            and: ["AuthorFilter", "RatingsFilter"]
+          }}
+        />
+        <MultiList
+            className="filter"
             componentId="AuthorFilter"
             dataField="authors.raw"
             title="Authors"
@@ -40,42 +39,39 @@ const App = () => (
             placeholder="Filter by author name"
             filterLabel="Authors"
             react={{
-              and: ["SearchBox", "RatingsFilter", "SearchResult"]
+              and: ["SearchBox", "RatingsFilter"]
             }}
           />
-        </div>
-        <div className="filter">
-          <SingleRange
-            componentId="RatingsFilter"
-            dataField="average_rating_rounded"
-            title="Book Ratings"
-            data={[
-              {
-                start: 4,
-                end: 5,
-                label: "★★★★ & up"
-              },
-              {
-                start: 3,
-                end: 5,
-                label: "★★★ & up"
-              },
-              {
-                start: 2,
-                end: 5,
-                label: "★★ & up"
-              },
-              {
-                start: 1,
-                end: 5,
-                label: "★ & up"
-              }
-            ]}
-            filterLabel="Ratings"
-          />
-        </div>
+        <SingleRange
+          className="filter"
+          componentId="RatingsFilter"
+          dataField="average_rating_rounded"
+          title="Book Ratings"
+          data={[
+            {
+              start: 4,
+              end: 5,
+              label: "★★★★ & up"
+            },
+            {
+              start: 3,
+              end: 5,
+              label: "★★★ & up"
+            },
+            {
+              start: 2,
+              end: 5,
+              label: "★★ & up"
+            },
+            {
+              start: 1,
+              end: 5,
+              label: "★ & up"
+            }
+          ]}
+          filterLabel="Ratings"
+        />
       </div>
-
       <div className="col">
         <SelectedFilters />
         <ReactiveList
@@ -83,53 +79,23 @@ const App = () => (
           dataField="original_title"
           size={10}
           className="result-list-container"
-          // pagination
           react={{
             and: ["SearchBox", "RatingsFilter", "AuthorFilter"]
           }}
-          render={({ data }) => (
-            <ReactiveList.ResultCardsWrapper>
-              {data.map(item => (
-                <ResultCard key={item.id}>
-                  <ResultCard.Image src={item.image} />
-                  <ResultCard.Title>
-                    <div
-                      className="book-title"
-                      dangerouslySetInnerHTML={{
-                        __html: item.original_title
-                      }}
-                    />
-                  </ResultCard.Title>
-
-                  <ResultCard.Description>
-                    <div className="flex column justify-space-between">
-                      <div>
-                        <div>
-                          by{" "}
-                          <span className="authors-list">{item.authors}</span>
-                        </div>
-                        <div className="ratings-list flex align-center">
-                          <span className="stars">
-                            {Array(item.average_rating_rounded)
-                              .fill("x")
-                              .map((item, index) => (
-                                <i className="fas fa-star" key={index} />
-                              )) // eslint-disable-line
-                            }
-                          </span>
-                          <span className="avg-rating">
-                            ({item.average_rating} avg)
-                          </span>
-                        </div>
-                      </div>
-                      <span className="pub-year">
-                        Pub {item.original_publication_year}
-                      </span>
-                    </div>
-                  </ResultCard.Description>
-                </ResultCard>
-              ))}
-            </ReactiveList.ResultCardsWrapper>
+          renderItem={item => (
+            <ResultList key={item._id}>
+              <ResultList.Image src={item.image} />
+              <ResultList.Content>
+                <ResultList.Title>
+                  {item.original_title}
+                </ResultList.Title>
+                <ResultList.Description>
+                  <div>by {item.authors}</div>
+                  <div>({item.average_rating} avg)</div>
+                  <span>Pub {item.original_publication_year}</span>
+                </ResultList.Description>
+              </ResultList.Content>
+            </ResultList>
           )}
         />
       </div>
